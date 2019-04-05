25aecac565e8e62a43653f508db3e1c40f07cd98
MarineLH <marine.lhuillier@epsi.fr>
2019-04-05 10:14:35 +0200

    Update FlaskServer and RedisServer files

--- FlaskServer.py
+++ FlaskServer.py
@@ -6,18 +6,21 @@ app = Flask(__name__)
 
 server_host = "127.0.0.1"
 server_port = 8080
+server_address = server_host + ":" + str(server_port)
 
 
 @app.route("/", methods=['GET'])
 def index():
     return "Utilisation du serveur : " \
            "\n\n - Récupérer toutes les notes : \n" \
-           "      GET http://" + server_host + ":" + str(server_port) + "/notes" \
+           "      GET http://" + server_address + "/notes" \
            "\n\n - Récupérer une note à partir de son id : \n" \
-           "      GET http://" + server_host + ":" + str(server_port) + "/notes/{id}" \
+           "      GET http://" + server_address + "/notes/{id}" \
            "\n\n - Créer une note : \n" \
-           "      POST http://" + server_host + ":" + str(server_port) + "/notes" \
-           "\n      avec dans le body le contenu de la note à créer"
+           "      POST http://" + server_address + "/notes" \
+           "\n      avec dans le body le contenu de la note à créer" \
+           "\n\n - Supprimer une note : \n" \
+           "      DELETE http://" + server_address + "/notes/{id}"
 
 
 @app.route("/notes", methods=['GET'])
@@ -30,28 +33,25 @@ def get_notes():
 def get_note(id):
     note = RedisServer.get_note(id)
     if note is None:
-        reponse = "Il n'existe aucune note avec l'id : " + id
+        return "Il n'existe aucune note avec l'id : " + id
     else:
-        reponse = "Récupération de la note " + id + " : \n    " + note
-
-    return reponse
+        return "Récupération de la note " + id + " : \n    " + note
 
 
 @app.route("/notes", methods=['POST'])
 def post_note():
-    id = uuid.uuid4().hex
-    RedisServer.post_note(str(id), str(request.data)[2:-1])
-    return "Création de la note : " + str(request.data)[2:-1] + "\nAvec l'id : " + str(id)
+    id = str(uuid.uuid4().hex)
+    note = str(request.data)[2:-1]
+    RedisServer.post_note(id, note)
+    return "Création de la note : " + note + "\nAvec l'id : " + id
 
 
 @app.route("/notes/<string:id>", methods=['DELETE'])
 def delete_note(id):
-    note = RedisServer.delete_note(id)
-    if note == 0:
-        reponse = "Impossible de supprimer la note avec l'id : " + id + " car elle n'existe pas"
+    if RedisServer.delete_note(id) == 0:
+        return "Impossible de supprimer la note avec l'id : " + id + " car elle n'existe pas"
     else:
-        reponse = "La note avec l'id : " + id + " a bien été supprimée"
-    return reponse
+        return "La note avec l'id : " + id + " a bien été supprimée"
 
 
 if __name__ == "__main__":
--- RedisServer.py
+++ RedisServer.py
@@ -4,34 +4,43 @@ redis_host = "localhost"
 redis_port = 6379
 redis_password = ""
 
+hash_name = "Notes"
+
 
 def get_note(id):
     try:
-        r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-        return r.hget("Notes", id)
+        r = connection_redis_server()
+        return r.hget(hash_name, id)
     except Exception as e:
         print(e)
 
 
 def post_note(id, msg):
     try:
-        r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-        return r.hset("Notes", id, msg)
+        r = connection_redis_server()
+        return r.hset(hash_name, id, msg)
     except Exception as e:
         print(e)
 
 
 def get_all_notes():
     try:
-        r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-        return r.hgetall("Notes")
+        r = connection_redis_server()
+        return r.hgetall(hash_name)
     except Exception as e:
         print(e)
 
 
 def delete_note(id):
     try:
-        r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-        return r.hdel("Notes", id)
+        r = connection_redis_server()
+        return r.hdel(hash_name, id)
     except Exception as e:
         print(e)
+
+
+def connection_redis_server():
+    try:
+        return redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+    except Exception as e:
+        return e
